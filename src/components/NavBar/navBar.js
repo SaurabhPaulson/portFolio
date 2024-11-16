b5070ac74006371bc26cf74670b2b05d6908a55e
SaurabhPaulson <saurabhpaulson@gmail.com>
2024-11-17 00:40:10 +0530

    experience changes

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-scroll'
 import contact from '../../assets/contact.png'
 import logo from '../../assets/logo.png'
 import './navBar.css';
+import menu from '../../assets/menu.png'
 
 const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <nav className="navbar">
         <img src={logo} alt="logo" className='logo' />
@@ -12,7 +14,7 @@ const NavBar = () => {
             <Link activeClass='active' to='home' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
             <Link activeClass='active' to='about' spy={true} smooth={true} offset={-60} duration={500} className="desktopMenuListItem">About Me</Link>
             <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Skills</Link>
-            <Link activeClass='active' to='home' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Experience</Link>
+            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Experience</Link>
             <Link activeClass='active' to='home' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Resume</Link>
 
         </div>
@@ -21,6 +23,16 @@ const NavBar = () => {
         }}>
             <img src = {contact} alt="contact" className="desktopMenuImg" />Contact Me
         </button>
+
+        <img src={menu} alt="Menu" className='mobMenu' onClick={() => setShowMenu(!showMenu)} />
+        <div className="navMenu" style={{display : showMenu ? 'flex': 'none'}} >
+            <Link activeClass='active' to='home' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Home</Link>
+            <Link activeClass='active' to='about' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={() => setShowMenu(false)}>About Me</Link>
+            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Skills</Link>
+            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Experience</Link>
+            <Link activeClass='active' to='home' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Resume</Link>
+            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={() => setShowMenu(false)}>Contact</Link>
+        </div>
 
     </nav>
     
